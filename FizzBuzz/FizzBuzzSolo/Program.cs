8cb6ab1d1423cfcdd39f1aff17f3e27ded48e580
Matt <Matt@DESKTOP-R42UFO5>
2016-04-25 11:08:01 +0100

    First upload of my completed independent project solution for the FizzBuzz problem

---
+++ FizzBuzz/FizzBuzzSolo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzSolo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int value = 1; value <= 100; value++)
+            {
+                string output = "";
+               
+                if (value % 3 == 0) output = "Fizz";
+                if (value % 5 == 0) output += "Buzz";
+                if (output == "") output = value.ToString();
+
+                Console.WriteLine(output);
+            }
+            Console.ReadLine();
+        }
+    }
+}
