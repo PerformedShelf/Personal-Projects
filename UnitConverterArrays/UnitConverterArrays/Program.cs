db8597beebe58e14d235b5cf798afe16132694b3
Matt <Matt@DESKTOP-R42UFO5>
2016-05-02 09:19:59 +0100

    Added menu and completed first conversion of kiometers to miles

--- UnitConverterArrays/UnitConverterArrays/Program.cs
+++ UnitConverterArrays/UnitConverterArrays/Program.cs
@@ -10,10 +10,46 @@ namespace UnitConverterArrays
     {
         static void Main(string[] args)
         {
-            double input = 4;
-            double mileConvert = 0.62137;
-            float miles = (float)input * (float)mileConvert;
-            Console.WriteLine(miles);
+            bool displayMenu = true;
+            while (displayMenu)
+            {
+                displayMenu = MainMenu();
+            }
+        }
+
+        private static bool MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Choose a conversion:");
+            Console.WriteLine("1) Kilometers to Miles");
+            Console.WriteLine("2) Exit");
+
+            string result = Console.ReadLine();
+            if (result == "1")
+            {
+                MileConvert();
+                return true;
+            }
+            else if (result == "2")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private static void MileConvert()
+        {
+            Console.Clear();
+            Console.WriteLine("Kilometer to Mile Conversion");
+            Console.Write("Type a measure in kilometers: ");
+            double input = double.Parse(Console.ReadLine());
+
+            double convert = 0.62137;
+            float miles = (float)input * (float)convert;
+            Console.WriteLine(input + " kilometers = " + miles + "miles");
             Console.ReadLine();
 
         }
